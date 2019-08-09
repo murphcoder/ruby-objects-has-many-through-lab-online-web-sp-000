0fa5412f8f327f76880679c3229ded80cd1d88f3
Sean Murphy <filmmakersean@fastmail.net>
2019-08-09 21:39:12 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -4,6 +4,15 @@ class Appointment
   
   @@all = []
   
-  def initialize()
+  def initialize(time,patient,doctor)
+    @time = time
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
   
 end
