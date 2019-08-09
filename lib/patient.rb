aff8e24a7d1833234a25d2ebdc0cf55cd31c9f4a
Sean Murphy <filmmakersean@fastmail.net>
2019-08-09 21:51:27 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -11,5 +11,14 @@ class Patient
   
   def new_appointment(doctor,time)
     Appointment.new(time,self,doctor)
+  end
+  
+  def appointments
+    Appointment.all.select {|appointment| appointment.patient == self}
+  end
+  
+  def doctors
+    appointments.collect {|appointment| appointment.doctor}
+  end
   
 end
