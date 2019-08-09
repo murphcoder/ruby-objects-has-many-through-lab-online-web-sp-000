3a9adc5b3a069f85f6734e7b3301aa2785ca6ea0
Sean Murphy <filmmakersean@fastmail.net>
2019-08-09 21:19:31 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,14 @@
 class Song
   
+  attr_reader :title, :artist, :genre
   
+  @@all = []
+  
+  def initialize(title,artist,genre)
+    @title = title
+    @artist = artist
+    @genre = genre
+    @@all << self
+  end
   
 end
